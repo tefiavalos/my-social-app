018e5f7be1575851a9c59c65aa0da43e49ae04ad
Estefania <emlavalos92@gmail.com>
2025-02-17 14:10:32 -0300

    fixing build errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  "compilerOptions": {
+    "baseUrl": ".",
+    "paths": {
+      "@/*": ["src/*"]
+    }
+  }
+}
+
 
 export default nextConfig;
