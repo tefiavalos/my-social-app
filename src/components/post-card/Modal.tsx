0dbea69fcb27164d3c07b3c69907e939e0625d8f
Estefania <emlavalos92@gmail.com>
2025-02-18 02:15:57 -0300

    fix modal

--- src/components/post-card/Modal.tsx
+++ src/components/post-card/Modal.tsx
@@ -28,13 +28,13 @@ const Modal: React.FC<Props> = ({
         className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50"
         onClick={() => setIsModalOpen(false)}
       >
-        <div className="relative p-4 max-w-full max-h-full">
+        <div className="relative p-4">
           <Image
             src={post.images[currentImage] ?? "/imagen1"}
             alt={`Imagen ${currentImage + 1}`}
-            className="w-auto h-auto max-w-screen max-h-screen object-contain rounded-xl"
-            width={350}
-            height={350}
+            className="object-contain rounded-xl"
+            width={500}
+            height={500}
             onClick={(e) => e.stopPropagation()}
           />
           {post.images.length > 1 && (
